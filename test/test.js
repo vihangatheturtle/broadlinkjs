4ad6fb7a1fca7501832846897fe5c31df48bec49
Dwan <me@dwaan.com>
2020-07-09 16:08:40 +0300

    Revert "Revert "Updateing test.js""
    This reverts commit 27ef3714ec2e7a7982307f3970740decf7225cc2.

--- test/test.js
+++ test/test.js
@@ -5,32 +5,13 @@ let fs = require('fs');
 var b = new broadlink();
 
 b.on("deviceReady", (dev) => {
-    if (dev.type == "RMPro") {
-        var timer = setInterval(function(){
-            console.log("send check!", dev.host.address, dev.type);
-            dev.checkData();
-        }, 1000);
+    console.log("Detected:", dev.host.address, dev.type);
 
-        dev.on("temperature", (temp)=>{
-            console.log("RMPRO: get temp " + temp, dev.host.address, dev.type);
-            dev.enterLearning();
-        });
-
-        dev.on("rawData", (data) => {
-            fs.writeFile("test1", data, function(err) {
-                if(err) {
-                    return console.log(err);
-                }
-                console.log("The file was saved!");
-                clearInterval(timer);
-            });
-        });
-        dev.checkTemperature();
-    } if (dev.type == "RM3" || dev.type == "RMMini") {
-        var timer = setInterval(function(){
-            console.log("RM: send check!", dev.host.address, dev.type);
-            dev.checkData();
-        }, 1000);
+    if (dev.type == "RMPro" || dev.type == "RM3" || dev.type == "RMMini") {
+        // var timer = setInterval(function(){
+        //     console.log("RM: send check!", dev.host.address, dev.type);
+        //     dev.checkData();
+        // }, 1000);
 
         dev.on("temperature", (temp)=>{
             console.log("get temp " + temp, dev.host.address, dev.type);
